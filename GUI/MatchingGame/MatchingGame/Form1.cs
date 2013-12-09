9966bfa9e46b2993b01f8cce261ceaacf0690965
thalcave <florinmmicu@gmail.com>
2013-12-09 11:59:56 +0100

    Visual C# tutorials: * PictureViewer: open a picture and display it * MathQuiz: generate random numbers for a simple quiz * MatchingGame: match icons game

---
+++ GUI/MatchingGame/MatchingGame/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    //When a player clicks one of the squares with a hidden icon, the program shows the icon to the player by changing the icon color to black.
+    //Then the player clicks another hidden icon.
+    //If the icons match, they stay visible. If not, both icons are hidden again.
+
+
+    public partial class Form1 : Form
+    {
+        //choose random icons for squares
+        Random random = new Random();
+
+        //list of icons, with each icon appearing twice in list
+        List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        //first clicked Label - or null
+        Label firstClicked = null;
+        //second clicked Label
+        Label secondClicked = null;
+
+        //currently discovered pairs of labels
+        int discoveredLabels = 0;
+        //total number of icon pairs
+        readonly int totalNumber;
+
+        //store the number of clicks
+        uint numberOfTries = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            totalNumber = icons.Count / 2;
+            AssignIconToSquares();
+        }
+
+        /// <summary> 
+        /// Assign each icon from the list of icons to a random square 
+        /// </summary> 
+        private void AssignIconToSquares()
+        {
+            foreach (var control in tableLayoutPanel1.Controls)
+            {
+                //convert control to label
+                Label currentLabel = (Label)control;
+                if (currentLabel != null)
+                {
+                    //pick a random icon
+                    int randomIcon = random.Next(0, icons.Count);
+                    currentLabel.Text = icons[randomIcon];
+                    currentLabel.ForeColor = currentLabel.BackColor;
+                    icons.RemoveAt(randomIcon);
+                }
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void labelClick(object sender, EventArgs e)
+        {
+            ++numberOfTries;
+            // The timer is only on after two non-matching  
+            // icons have been shown to the player,  
+            // so ignore any clicks if the timer is running 
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null)
+            {
+                // If the clicked label is black, the player clicked 
+                // an icon that's already been revealed -- 
+                // ignore the click 
+                if (clickedLabel.ForeColor == Color.Black)
+                    return;
+
+                // If firstClicked is null, this is the first icon 
+                // in the pair that the player clicked,  
+                // so set firstClicked to the label that the player  
+                // clicked, change its color to black, and return 
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    firstClicked.ForeColor = Color.Black;
+                    return;
+                }
+
+                // If the player gets this far, the timer isn't 
+                // running and firstClicked isn't null, 
+                // so this must be the second icon the player clicked 
+                // Set its color to black
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                //we have a match
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+
+                    //make a sound
+                    System.Media.SystemSounds.Exclamation.Play();
+
+                    ++discoveredLabels;
+                    if (totalNumber == discoveredLabels)
+                    {
+                        MessageBox.Show("Congratulations, you finished in : " + numberOfTries.ToString(), "Wow");
+                        this.Close();
+                    }
+
+                    return;
+                }
+
+                //play a sound
+                System.Media.SystemSounds.Beep.Play();
+
+                // If the player gets this far, the player  
+                // clicked two different icons, so start the  
+                // timer (which will wait three quarters of  
+                // a second, and then hide the icons)
+                timer1.Start();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Stop the timer
+            timer1.Stop();
+
+            // Hide both icons
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            //play sound
+            System.Media.SystemSounds.Asterisk.Play();
+
+            // Reset firstClicked and secondClicked  
+            // so the next time a label is 
+            // clicked, the program knows it's the first click
+            firstClicked = null;
+            secondClicked = null;
+        }
+    }
+}
---
+++ GUI/MathQuiz/MathQuiz/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MathQuiz
+{
+    public partial class Form1 : Form
+    {
+        //initialize a new instance of Random class
+        Random randomizer = new Random();
+
+        //use these 2 for addition problem
+        int addend1;
+        int addend2;
+
+        //subtraction problem
+        int minuend;
+        int subtrahend;
+
+        //multiplication problem
+        int leftFactor;
+        int rightFactor;
+
+        //division
+        int dividend;
+        int divisor;
+
+        //Keep track of remaining time
+        int timeLeft;
+
+        const int initialTime = 20;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Start the quiz by filling in all of the problems 
+        /// and starting the timer. 
+        /// </summary> 
+        public void StartTheQuiz()
+        {
+            //Fill-in the addition problem
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+
+            plusLeftLabel.Text = Convert.ToString(addend1);
+            plusRightLabel.Text = Convert.ToString(addend2);
+
+            //initialize it to 0
+            sum.Value = 0;
+
+            minuend = randomizer.Next(51);
+            subtrahend = randomizer.Next(51);
+            minusLeftLabel.Text = Convert.ToString(minuend);
+            minusRightLabel.Text = Convert.ToString(subtrahend);
+            difference.Value = 0;
+
+            leftFactor = randomizer.Next(10);
+            rightFactor = randomizer.Next(15);
+            timesLeftLabel.Text = leftFactor.ToString();
+            timesRightLabel.Text = rightFactor.ToString();
+            product.Value = 0;
+
+            dividend = randomizer.Next(51);
+            divisor = randomizer.Next(1, 51);
+            dividedLeftLabel.Text = dividend.ToString();
+            dividedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            timeLeft = initialTime;
+            timeLabel.Text = initialTime.ToString() + " seconds";
+            timer1.Start();
+        }
+
+        private void quotient_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void ResetQuiz()
+        {
+            startButton.Enabled = true;
+            timeLabel.BackColor = default(Color);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //a value of 1000 causes the Tick event to fire every second.
+
+            if (CheckSum() && CheckDiff() && CheckMultiplication() && CheckDivision())
+            {
+                // If all functions return true, then the user  
+                // got the answer right. Stop the timer   
+                // and show a MessageBox.
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!", "Congratulations!");
+
+                ResetQuiz();
+            }
+            else if (timeLeft > 0)
+            {
+                          
+                //Display the new time left
+                --timeLeft;
+                timeLabel.Text = timeLeft.ToString();
+
+                if (timeLeft == 5)
+                {
+                    timeLabel.BackColor = Color.Red;
+                }
+            }
+            else
+            {
+                //ran out of time --> display a MessageBox
+                //stop the timer
+                timer1.Stop();
+
+                timeLabel.Text = "Time's up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+
+                ResetQuiz();
+
+            }
+        }
+
+        /// <summary> 
+        /// Check the answer to see if the user got everything right. 
+        /// </summary> 
+        /// <returns>True if the answer's correct, false otherwise.</returns> 
+        private bool CheckSum()
+        {
+            //MessageBox.Show("Check sum", "Congratulations!");
+            return addend1 + addend2 == sum.Value;
+        }
+
+        private bool CheckDiff()
+        {
+            //MessageBox.Show("Check diff", "Congratulations!");
+            return minuend - subtrahend == difference.Value;
+        }
+
+        /// <summary> 
+        /// Check if multiplication is OK
+        /// </summary> 
+        /// <returns>True if the answer's correct, false otherwise.</returns> 
+        private bool CheckMultiplication()
+        {
+            //MessageBox.Show("Check mul", "Congratulations!");
+            return leftFactor * rightFactor == product.Value;
+        }
+
+        /// <summary>
+        /// Check if division is OK
+        /// </summary>
+        /// <returns>True if result is correct, false otherwise
+        private bool CheckDivision()
+        {
+            //MessageBox.Show("Check div", "Congratulations!");
+            return dividend / divisor == quotient.Value;
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            //sender = object whose event is firing
+            NumericUpDown answerBox = sender as NumericUpDown;  //cast sender to NumericUpDown
+            if (answerBox != null)  //conversion succeeded
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length; //get length of answer
+                answerBox.Select(0, lengthOfAnswer);    //select current value based on length: text in NumericUpDown will be selected when entering it
+            }
+        }
+    }
+}
